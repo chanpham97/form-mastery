980f36079d5b367f8a6d2379c6e238185ce4807a
Chan Pham <iamchanpham@gmail.com>
2025-06-05 14:46:12 +0000

    mastery template

---
+++ script.js
@@ -0,0 +1,10 @@
+// Select the button and inputs
+
+
+// Write the event listener that, when the button in clicked,
+// Loops through all the inputs to check if the FIRST question was answered correctly
+// You should check the question value, name, and if it was selected
+
+
+
+
